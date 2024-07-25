7ef106b2a399710c30c41fd839a7109fb1397bd2
ycostard <yohann.costard.dev@gmail.com>
2024-07-25 15:31:43 +0200

    Msie en place des mails

--- src/models/Annonce.js
+++ src/models/Annonce.js
@@ -6,18 +6,38 @@ const Annonce = {
     count() {
         return prisma.annonce.count();
     },
-    findAllByoffsetAndLimit(offset, limit) {
-        return prisma.annonce.findMany({
-          skip: parseInt(offset),
-          take: parseInt(limit),
-          orderBy: {
-            date_creation: "desc",
-          },
-            include: {
-                vehicule: true,
-            },
-        });
-      },
+    findAllByoffsetAndLimit(offset, limit, search) {
+      const where = {};
+    
+      // Ajout des filtres pour le champ "search"
+      if (search) {
+        where.OR = [
+          { rue: { contains: search } },
+          { ville: { contains: search } },
+          { pays: { contains: search } },
+          { vehicule: {
+            OR: [
+              { marque: { contains: search }},
+              { modele: { contains: search } },
+              { couleur: { contains: search} },
+              { carburant: { contains: search } }
+            ]
+          }}
+        ];
+      }
+    
+      return prisma.annonce.findMany({
+        skip: parseInt(offset),
+        take: parseInt(limit),
+        where,
+        orderBy: {
+          date_creation: 'desc'
+        },
+        include: {
+          vehicule: true
+        }
+      });
+    },      
 
     create(annonceData) {
         return prisma.annonce.create({
